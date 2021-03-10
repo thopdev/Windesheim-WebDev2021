3b0e53c122480f65350b96d14b54a45c19d2ae82
Thomas Hop <thomas.hop@pw.com>
2021-03-10 11:41:17 +0100

    Adding a TodoList Component

---
+++ Windesheim.WebDev.Blazor/Pages/TodoListComponent.razor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Windesheim.WebDev.Blazor.Pages
+{
+    public partial class TodoListComponent
+    {
+        public List<string> Tasks { get; set; } = new List<string>
+        {
+            "Add input form",
+            "Add local storage"
+        };
+    }
+}
